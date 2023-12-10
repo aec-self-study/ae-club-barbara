aca194ca9e93f7150d9f51162ca63420082020b7
Barbara Dekeyser <89662612+bardeke@users.noreply.github.com>
2023-12-10 01:01:59 +0000

    Add customers query

---
+++ customers.sql
@@ -0,0 +1,22 @@
+
+WITH temp_orders_customers AS (
+  SELECT
+  o.customer_id AS customer_id,
+  name AS customer_name,
+  email AS customer_email,
+  created_at AS order_created_at,
+  o.id AS order_id
+FROM analytics-engineers-club.coffee_shop.customers as c
+LEFT JOIN analytics-engineers-club.coffee_shop.orders as o
+ON c.id= o.customer_id
+)
+SELECT
+  customer_id,
+  customer_name AS name,
+  customer_email AS email,
+  MIN(order_created_at) AS first_order_at,
+  COUNT(DISTINCT order_id) AS number_of_orders
+FROM temp_orders_customers
+GROUP BY 1,2,3
+ORDER BY first_order_at
+LIMIT 5;
